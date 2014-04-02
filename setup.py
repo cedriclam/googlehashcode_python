6ead008d55d2431b72cc06d1a183b1867b028dad
Cedric Lamoriniere <cedric.lamoriniere@gmail.com>
2014-04-02 23:00:11 +0200

    init project

---
+++ setup.py
@@ -0,0 +1,11 @@
+'''
+Created on Apr 2, 2014
+
+@author: clamoriniere
+'''
+from setuptools import setup
+setup(name='googlehashcode',
+      version='1.0',
+      py_modules=['src.googlehashcode'],
+      test_suite="tests", 
+      )
---
+++ src/googlehashcode.py
@@ -0,0 +1,16 @@
+'''
+Created on Apr 2, 2014
+
+@author: clamoriniere
+'''
+
+import sys
+
+
+if __name__ == '__main__':
+    for line in sys.stdin:
+        print line
+        
+    sys.stdout.write("blabla")
+
+    pass
